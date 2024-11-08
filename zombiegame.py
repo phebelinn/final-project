e2b8c7c750d297b07bf6a708a720747ad0c32d98
phebelinn <phebelinndean@gmail.com>
2024-11-08 16:56:19 -0600

    set up screen display

--- zombiegame.py
+++ zombiegame.py
@@ -1,9 +1,15 @@
 import pygame
 
+#define walking
+
+#set up display window with map
+
 def main():
     pygame.init()
     pygame.display.set_caption("Zombie Grrrlz")
     running = True
+    resolution = (800, 600)
+    screen = pygame.display.set_mode(resolution)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
