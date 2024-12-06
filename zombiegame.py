4c47e0ae33bb430aa93f7f175f41c2a99e377783
phebelinn <phebelinndean@gmail.com>
2024-12-05 18:18:00 -0600

    fixed player return to spawn

--- zombiegame.py
+++ zombiegame.py
@@ -23,15 +23,15 @@ class Player():
          #run through images for animation 
          for num in range(0, 3):
              img_forward = pygame.image.load(f'img/zombforward{num}.png')
-             img_forward = pygame.transform.scale(img_forward, (40, 60))
+             img_forward = pygame.transform.scale(img_forward, (120, 150))
              self.images_forward.append(img_forward)
          for num in range(1, 5):
              img_backward = pygame.image.load(f'img/zombbackward{num}.png')
-             img_backward = pygame.transform.scale(img_backward, (40, 60))
+             img_backward = pygame.transform.scale(img_backward, (120, 150))
              self.images_backward.append(img_backward)
          self.image = self.images_forward[self.index]    
          img = pygame.image.load('img/zombforward1.png')
-         self.image = pygame.transform.scale((40, 60))
+         self.image = pygame.transform.scale(img, (120, 150))
          self.rect = self.image.get_rect()
          self.rect.x = x
          self.rect.y = y 
@@ -50,8 +50,7 @@ class Player():
         if key[pygame.K_s]:
             self.rect.y += 5
             self.counter += 1
-        if key[pygame.K_a] == False and key[pygame.K_d] == False and key[pygame.K_w] == False and key[pygame.K_s] == False:
-            self.counter = 0 
+        if key[pygame.K_a] == False and key[pygame.K_d] == False and key[pygame.K_w] == False and key[pygame.K_s] == False: 
             self.index = 0
         #adjust bounds
         if self.rect.bottom > screen_height:
@@ -80,7 +79,7 @@ def display_game(screen, player, map_number):
            
 
 def main():
-
+    player = Player(100, screen_height - 250)
     running = True
     while running:
         clock.tick(fps)
@@ -89,7 +88,6 @@ def main():
                 running = False
         map_number = 1
         display_game(screen, Player, map_number)
-        player = Player(100, screen_height - 250)
         pygame.display.flip()
         player.update()
 
